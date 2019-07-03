30a71a17b676c4b212ace54b03a047d8d603c1aa
dgilleland <dgilleland@nait.ca>
2019-07-02 21:46:18 -0600

    Security Demo

--- src/Identity/Security Sample/SecuritySample/Admin/Security/Settings.cs
+++ src/Identity/Security Sample/SecuritySample/Admin/Security/Settings.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web;
 
-namespace WebApp.Admin.Security
+// You can learn about Database Initialization Strategies in EF6 via
+// http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
+
+namespace SecuritySample.Admin.Security
 {
     /// <summary>
     /// A collection of application wide settings that provide values which
